4c218a79a8c7b2a561c70cbc67918758609d80f5
energizeH <Hxssxn772@gmail.com>
2025-06-11 09:38:02 +0100

    Create index-Dzg2wHSu.js

---
+++ assets/assets/index-Dzg2wHSu.js
@@ -0,0 +1,10 @@
+import __vite__cjsImport0_react_jsxDevRuntime from "/node_modules/.vite/deps/react_jsx-dev-runtime.js?v=03370321"; const jsxDEV = __vite__cjsImport0_react_jsxDevRuntime["jsxDEV"];
+import __vite__cjsImport1_react from "/node_modules/.vite/deps/react.js?v=03370321"; const React = __vite__cjsImport1_react["default"];
+import __vite__cjsImport2_reactDom_client from "/node_modules/.vite/deps/react-dom_client.js?v=03370321"; const ReactDOM = __vite__cjsImport2_reactDom_client["default"];
+import App from "/src/App.jsx";
+import "/src/index.css";
+import { BrowserRouter } from "/node_modules/.vite/deps/react-router-dom.js?v=03370321";
+import { inject } from "/node_modules/.vite/deps/@vercel_analytics.js?v=03370321";
+inject( );
+ReactDOM.createRoot(document.getElementById("root")).render(jsxDEV(React.StrictMode, { children: jsxDEV(BrowserRouter, { children: jsxDEV(App, {}, void 0, false, { fileName: _jsxFileName, lineNumber: 10, columnNumber: 11 }, this) }, void 0, false, { fileName: _jsxFileName, lineNumber: 9, columnNumber: 9 }, this) }, void 0, false, { fileName: _jsxFileName, lineNumber: 8, columnNumber: 8 }, this));
+
